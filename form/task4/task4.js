6481b6bbc9611df8c9b9c84f52493ad12d0f0544
Tianshu <tianshu9566@gmail.com>
2017-03-14 13:53:24 +0800

    update

--- form/task4/task4.js
+++ form/task4/task4.js
@@ -3,6 +3,7 @@ window.onload = function() {
 	var horizontal = 0;
 	var vertical = 0;
 	var check = 0;
+	var state = 0;
 	for (var i = 0; i < 10; i++) { //row
 		for (var j = 0; j < 10; j++) { //coloum
 			var grid = document.createElement("div");
@@ -16,51 +17,110 @@ window.onload = function() {
 	document.onkeydown = function(e) {
 		if (e.keyCode == 39 && check == 0) {
 			check = 1;
-			console.log(check);
 			var left = cube.offsetLeft;
 			left += 50;
-			horizontal
-				+= 90;
-			cube.style.transformOrigin = "50% 50% 25px";
-			cube.style.transform = 'rotateY(' + horizontal + 'deg)';
+			var initial = (cube.style.transform).match(/-?\d{1,3}/g);
+			var initialX = initial[0];
+			var initialY = initial[1];
+			var initialZ = initial[2];
+			if (state==1) {
+				var finalZ = parseInt(initialZ)-90;
+				cube.style.transform = 'rotateX(' + initialX + 'deg) rotateY(' + initialY + 'deg) rotateZ('+finalZ+'deg)';
+			}else if(state==2){
+				var finalY = parseInt(initialY)-90;
+				cube.style.transform = 'rotateX(' + initialX + 'deg) rotateY(' + finalY + 'deg) rotateZ('+initialZ+'deg)';
+			}else if(state==3){
+				var finalZ = parseInt(initialZ)+90;
+				cube.style.transform = 'rotateX(' + initialX + 'deg) rotateY(' + initialY + 'deg) rotateZ('+finalZ+'deg)';
+			}else{
+			var finalY = parseInt(initialY)+90;
+			cube.style.transform = 'rotateX(' + initialX + 'deg) rotateY(' + finalY + 'deg) rotateZ('+initialZ+'deg)';
+			}
 			cube.style.left = left + "px";
+						console.log(cube.style.transform);
 			setTimeout(function() {
 				check = 0;
-			}, 1000);
+			}, 200);
 		} else if (e.keyCode == 37 && check == 0) {
 			check=1;
 			var left = cube.offsetLeft;
 			left -= 50;
-			horizontal
-				-= 90;
-			cube.style.transformOrigin = "50% 50% 25px";
-			cube.style.transform = 'rotateY(' + horizontal + 'deg)';
+			var initial = (cube.style.transform).match(/-?\d{1,3}/g);
+			var initialX = initial[0];
+			var initialY = initial[1];
+			var initialZ = initial[2];
+			var finalY = parseInt(initialY)-90;
+			cube.style.transform = 'rotateX(' + initialX + 'deg) rotateY(' + finalY + 'deg) rotateZ('+initialZ+'deg)';
 			cube.style.left = left + "px";
 			setTimeout(function() {
 				check = 0;
-			}, 1000);
+			}, 200);
 		} else if (e.keyCode == 38&&check==0) {
 			check=1;
+			state+=1;
+			if(state==4){
+				state=0;
+			}
 			var top = cube.offsetTop;
 			top -= 50;
-			vertical+=90;
-			cube.style.transformOrigin = "50% 50% 25px";
-			cube.style.transform = 'rotateX(' + vertical + 'deg)';
+			var initial = (cube.style.transform).match(/-?\d{1,3}/g);
+			var initialX = initial[0];
+			var initialY = initial[1];
+			var initialZ = initial[2];
+			var finalX = parseInt(initialX)+90;
+			cube.style.transform = 'rotateX(' + finalX + 'deg) rotateY('+initialY+'deg) rotateZ('+initialZ+'deg)';
 			cube.style.top = top + "px";
+			console.log(cube.style.transform);
 			setTimeout(function() {
 				check = 0;
-			}, 1000);
+			}, 200);
 		}else if(e.keyCode==40&&check==0){
 			check=1;
+			state-=1;
+			if(state==-4){
+				state=0;
+			}
+			console.log(cube.style.transform);
 			var top = cube.offsetTop;
 			top += 50;
-			vertical-=90;
-			cube.style.transformOrigin = "50% 50% 25px";
-			cube.style.transform = 'rotateX(' + vertical + 'deg)';
+			var initial = (cube.style.transform).match(/-?\d{1,3}/g);
+			var initialX = initial[0];
+			var initialY = initial[1];
+			var initialZ = initial[2];
+			var finalX = parseInt(initialX)-90;
+			cube.style.transform = 'rotateX(' + finalX + 'deg) rotateY('+initialY+'deg) rotateZ('+initialZ+'deg)';
 			cube.style.top = top + "px";
 			setTimeout(function() {
 				check = 0;
-			}, 1000);
+			}, 200);
 		}
-	}
+	};
+	// function chooseTransform(state){
+	// 	switch (state) {
+	// 		case 1:
+				
+	// 			break;
+	// 		case 2:
+
+	// 			break;
+	// 		case 3:
+
+	// 			break;
+	// 		case 4:
+
+	// 			break;
+	// 		case -1:
+
+	// 			break;
+	// 		case -2:
+
+	// 			break;
+	// 		case -3:
+
+	// 			break;
+	// 		case -4:
+
+	// 			break;
+	// 	}
+	// };
 }
